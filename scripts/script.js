a3271678b7306aa54f347812149526430aa99ed1
Shomnath Pramanik <shomnathcse22@gmail.com>
2021-10-21 23:07:21 +0600

    Add jQuery selectors

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     alert("jQuery Loaded!");
 });
+
+// jQuery Selectors
+$("h3").css({border: "1px solid blue"});
+$(".wrapper").css({border: "1px solid red"});
+$("#clients").css({border: "1px solid green"});
