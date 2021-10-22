14c8ab4aeb5bbf6111a8dd09b8c88e4e77f9ee45
Shomnath Pramanik <shomnathcse22@gmail.com>
2021-10-22 22:38:02 +0600

    Add jQuery selectors

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,36 @@
-$(document).ready(function(){
-    alert("jQuery Loaded!");
-});
 
-// jQuery Selectors
-$("h3").css({border: "1px solid blue"});
-$(".wrapper").css({border: "1px solid red"});
-$("#clients").css({border: "1px solid green"});
+// //////////////////////////////
+// ////// jQuery Load Test //////
+// //////////////////////////////
+
+// $(document).ready(function(){
+//     alert("jQuery Loaded!");
+// });
+
+// //////////////////////////////
+// ////// jQuery Selectors //////
+// //////////////////////////////
+
+// $("h3").css({border: "1px solid blue"});
+// $(".wrapper").css({border: "1px solid red"});
+// $("#clients").css({border: "1px solid green"});
+
+// jQuery Filters
+// $("header nav li:first").css({border: "2px solid red"});
+// $("header nav li:last").css({border: "2px solid red"});
+
+// $("#contact h2:first-child").css({border: "2px solid blue"});
+// $("#contact ul:first-child").css({border: "2px solid blue"});
+// $("#contact ul:last").css({border: "2px solid blue"});
+
+// $("header nav li:even").css({border: "2px solid blue"});
+// $("header nav li:odd").css({border: "2px solid yellow"});
+
+// $("section:not('#contact')").css({border: "2px solid green"});
+
+// $("#social-nav li:lt(2)").css({background: "rgba(0, 0, 255, 0.4)"});
+// $("#social-nav li:gt(2)").css({background: "rgba(0, 255, 0, 0.5)"});
+
+// $("div[class]").css({border: "2px solid pink"});
+// $("img[alt=e-commerce]").css({border: "2px solid red"});
+
